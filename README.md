207c00d2d72338ade4949e4d2eb68ed4b100632b
Emre <emregultekin2022@gmail.com>
2026-02-06 23:44:50 +0300

    Exercise 1: Character Input

---
+++ README.md
@@ -0,0 +1,16 @@
+# Practice Python Exercises - PracticePython.org
+This repository contains complete solutions to all exercises from www.practicepython.org
+
+
+## Exercises List
+
+- [x] Exercise 1: Character Input
+- [x] Exercise 2: Odd Or Even  
+- [x] Exercise 3: List Less Than Ten
+- [x] Exercise 4: Divisors
+- [x] Exercise 5: List Overlap
+
+## How to Run?
+
+
+
---
+++ solutions/ex01_character_input.py
@@ -0,0 +1,37 @@
+"""
+Exercise 1: Character Input
+
+Create a program that asks the user to enter their name and their age. 
+Print out a message addressed to them that tells them the year that 
+they will turn 100 years old.
+
+Extras:
+
+Add on to the previous program by asking the user for another number 
+and printing out that many copies of the previous message. 
+Print out that many copies of the previous message on separate lines. 
+"""
+from datetime import datetime
+
+
+def calculate_100_year():
+    """
+    Calculate the year when someone turns 100 years old.
+
+    Args:
+
+    Returns:
+        int: The year when the person will be 100 years old    
+
+    """
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+    count = int(input("Enter message count: "))
+
+    current_year = datetime.now().year
+    when_100_year = current_year + (100 - age)
+    for _ in range(count):
+        print(f"{name} will be 100 years old in {when_100_year}")
+
+    print(f"Message printed {count} times.")
+    return when_100_year
